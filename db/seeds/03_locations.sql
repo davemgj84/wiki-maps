ed6d46225d22805af64fccc349d140951036c542
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-09-19 19:51:18 +0000

    edited locations and favorites tables

--- db/seeds/03_locations.sql
+++ db/seeds/03_locations.sql
@@ -1,8 +1,7 @@
-INSERT INTO locations (map_id, title, description, image_url, latitude, longitude)
-VALUES (1, 'The Hyde', 'Enter Jekyll, Leave Hyde', 'https://s3-media0.fl.yelpcdn.com/bphoto/eAPklWJqXYD6mwXgxNOE_Q/348s.jpg', 49.258610, -123.101060),
-VALUES (1, 'Buckstop', 'Small bar near by', 'https://foodpersuasiondotcom.files.wordpress.com/2014/01/2014-01-26-12-37-29.jpg', 49.291040, -123.136390),
-VALUES (1, 'The Blarney Stone', 'Party place', 'https://s3-media0.fl.yelpcdn.com/bphoto/KtTR_fy0UIdyGxvKN64fOg/l.jpg', 49.282921, -123.104233),
+INSERT INTO locations (map_id, title, description, image_url, latitude, longitude) VALUES (1, 'The Hyde', 'Enter Jekyll, Leave Hyde', 'https://s3-media0.fl.yelpcdn.com/bphoto/eAPklWJqXYD6mwXgxNOE_Q/348s.jpg', 49.258610, -123.101060);
+INSERT INTO locations (map_id, title, description, image_url, latitude, longitude) VALUES (1, 'Buckstop', 'Small bar near by', 'https://foodpersuasiondotcom.files.wordpress.com/2014/01/2014-01-26-12-37-29.jpg', 49.291040, -123.136390);
+INSERT INTO locations (map_id, title, description, image_url, latitude, longitude) VALUES (1, 'The Blarney Stone', 'Party place', 'https://s3-media0.fl.yelpcdn.com/bphoto/KtTR_fy0UIdyGxvKN64fOg/l.jpg', 49.282921, -123.104233);
 
-VALUES (2, 'The Naam', '24 hour veggie place', 'https://spacing.ca/vancouver/wp-content/uploads/sites/6/Naam1-e1346021899632.jpg', 49.268200, -123.166960),
-VALUES (2, 'Kazami', 'Good sushi', 'https://www.kazami.ca/uploads/1/2/5/1/125190357/published/img-0673-2.jpg?1556129742', 49.288810, -123.131220),
-VALUES (2, 'La Taqueria', 'Best tacos!', 'https://www.straight.com/files/v3/styles/gs_standard/public/images/19/10/la_taqueria_taco_day.jpg?itok=DiSX8zbR', 49.263430, -123.112820),
+INSERT INTO locations (map_id, title, description, image_url, latitude, longitude) VALUES (2, 'The Naam', '24 hour veggie place', 'https://spacing.ca/vancouver/wp-content/uploads/sites/6/Naam1-e1346021899632.jpg', 49.268200, -123.166960);
+INSERT INTO locations (map_id, title, description, image_url, latitude, longitude) VALUES (2, 'Kazami', 'Good sushi', 'https://www.kazami.ca/uploads/1/2/5/1/125190357/published/img-0673-2.jpg?1556129742', 49.288810, -123.131220);
+INSERT INTO locations (map_id, title, description, image_url, latitude, longitude) VALUES (2, 'La Taqueria', 'Best tacos!', 'https://www.straight.com/files/v3/styles/gs_standard/public/images/19/10/la_taqueria_taco_day.jpg?itok=DiSX8zbR', 49.263430, -123.112820);
--- db/seeds/04_favorites.sql
+++ db/seeds/04_favorites.sql
@@ -1,3 +1,2 @@
-INSERT INTO favorites (user_id, map_id) 
-VALUES (1, 1),
-VALUES (1, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (1, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (1, 2);
