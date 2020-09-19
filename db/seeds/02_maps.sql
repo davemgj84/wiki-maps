b8e7edb9f78ffccdc67cf0b1d6bc2f92bee69fc5
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-09-19 19:48:28 +0000

    edited maps table

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,4 @@
 -- Widgets table seeds here (Example)
-INSERT INTO maps (user_id, title, description)
-VALUES (1, 'bars', 'favorite bars'),
-VALUES (1, 'restaurants', 'favorite places to eat');
+INSERT INTO maps (user_id, title, description) VALUES (1, 'bars', 'favorite bars');
+INSERT INTO maps (user_id, title, description) VALUES (1, 'restaurants', 'favorite places to eat');
 
