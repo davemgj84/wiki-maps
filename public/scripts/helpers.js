6dd700a99b842051b1da09592329b13ff45b7b21
davemgj84 <davemgj@gmail.com>
2020-09-24 21:00:56 +0000

    started on edit function

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -4,9 +4,7 @@ const emptyContainer = () => {
 };
 
 const createMarkers = (markers) => {
-  console.log('Markers:', markers)
   for (const coords of markers) {
-  console.log('coords:', coords[2])
     let contentString = `<h1>a town</h1><img src="${coords[2]}">`;
     let latLng = new google.maps.LatLng(coords[0], coords[1]);
     let marker = new google.maps.Marker({
@@ -26,7 +24,6 @@ const createMarkers = (markers) => {
 
 const loadLocations = (id) => {
   $.get(`/maps/${id}/locations`, (res) => {
-    console.log('res:', res)
     for (const latlong of res.maps) {
       markers.push([latlong.latitude, latlong.longitude]);
     }
@@ -37,7 +34,6 @@ const getLocations = (target) => {
   const $getLocation = $(target);
   $getLocation.on('click', 'button', function(event) {
     markers = [];
-    console.log("markers inside getlocations:", markers)
     loadLocations(event.target.id);
     setTimeout(() => {
       $(() => {
@@ -51,3 +47,21 @@ const getLocations = (target) => {
     }, 100);
   });
 };
+
+// const getEditLocations = (target) => {
+//   const $getLocation = $(target);
+//   $getLocation.on('click', '.edit', function(event) {
+//     markers = [];
+//     loadLocations(event.target.id);
+//     setTimeout(() => {
+//       $(() => {
+//         const options = {
+//           zoom: 12,
+//           center: { lat: 49.259660, lng: -123.107220 }
+//         };
+//         map = new google.maps.Map($('#map').get(0), options);
+//         createMarkers(markers);
+//       });
+//     }, 100);
+//   });
+// };
--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,9 +1,9 @@
 function initMap() {
-    const options = {
-      zoom: 12,
-      center: { lat: 49.259660, lng: -123.107220 }
-    };
-    map = new google.maps.Map($('#map').get(0), options);
+  const options = {
+    zoom: 12,
+    center: { lat: 49.259660, lng: -123.107220 }
+  };
+  map = new google.maps.Map($('#map').get(0), options);
 
   const input = document.getElementById('search');
   const searchBox = new google.maps.places.SearchBox(input);
@@ -26,20 +26,21 @@ function initMap() {
     // console.log('places', places)
     //if we find that no other places were found meaning that the array is empty then we don't
     //want to do any other work with places.length:
-    if(places.length === 0)
-    return;
+    if (places.length === 0)
+      return;
     //otherwise we want to continue on
     markers.forEach((currentMarker) => {
       //to get rid of the map reference within that market
-      currentMarker.setMap(null); });
-      // markers = [];
-      // console.log('markers1:', markers)
-      // console.log('places2', places)
+      currentMarker.setMap(null);
+    });
+    // markers = [];
+    console.log('markers1:', markers)
+    console.log('places2', places)
 
 
     //coodrinate boundaries of a map
     const bounds = new google.maps.LatLngBounds();
-    places.forEach(function (p) {
+    places.forEach(function(p) {
       if (!p.geometry) {
         return;
       }
@@ -48,28 +49,29 @@ function initMap() {
         map: map,
         title: p.name,
         position: p.geometry.location,
-        draggable:true
+        draggable: true
       })
 
-      google.maps.event.addListener(markers, 'dragend', function () {
+      google.maps.event.addListener(markers, 'dragend', function() {
         console.log('new', markers.getPosition().lat())
         console.log('new', markers.getPosition().lng())
       })
       console.log('markers3:', markers)
-      console.log('markers3:', markers.position.lat())
-      console.log('markers3:', markers.position.lng())
+      console.log('LAT:', markers.position.lat())
+      console.log('LONG:', markers.position.lng())
       console.log('p:', p.formatted_address)
 
       if (p.geometry.viewport) {
         // Extends this bounds to contain the union of this and the given bounds.
         bounds.union(p.geometry.viewport);
       } else {
-        bounds.extend(p.geometry.location)}
+        bounds.extend(p.geometry.location)
+      }
 
 
 
-      });
-      // adjust the viewport of the map
+    });
+    // adjust the viewport of the map
     map.fitBounds(bounds);
   })
 }
